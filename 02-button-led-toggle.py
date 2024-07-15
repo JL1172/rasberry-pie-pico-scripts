041f19f72ebd728120ab195645d8bb94a1743223
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-07-14 20:31:12 -0400

    fixed button script

--- 02-button-led-toggle.py
+++ 02-button-led-toggle.py
@@ -6,14 +6,11 @@ red_led = machine.Pin(15, machine.Pin.OUT)
 
 def toggle_led():
     red_led.toggle()
-    
-counter = 0
 
 while True:
-    if button.value() == 1:
-        print("You pressed the button!")
-        print(button.value())
-        print(counter)
-        counter+=1
+    if button.value() == 0:
         toggle_led()
         utime.sleep_ms(250)
+    
+        
+
